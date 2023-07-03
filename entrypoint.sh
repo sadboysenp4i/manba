1fa685cf4fd0a2ed09383e2a1d40a39f897c3176
sadboysenp4i <130453713+sadboysenp4i@users.noreply.github.com>
2023-07-03 22:08:59 +0300

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+set -e
+
+start_etcd() {
+    etcd $ETCD_OPTS &
+}
+
+DEFAULT_IP="0.0.0.0"
+
+start_apiserver() {
+    manba-apiserver --addr=${DEFAULT_IP}:9092 --addr-http=${DEFAULT_IP}:9093 --discovery $API_SERVER_OPTS &
+}
+
+INPUT_CMD=$@
+CMD=`cat cmd`
+if [ "$INPUT_CMD" = "" ]
+then
+    INPUT_CMD=${CMD}
+fi
+
+DEFAULT_EXEC="manba-proxy --addr=${DEFAULT_IP}:80 --log-level=$MANBA_LOG_LEVEL $GW_PROXY_OPTS"
+if [ "${INPUT_CMD}" = 'demo' ]
+then
+    start_etcd
+    sleep 3
+    start_apiserver
+    sleep 1
+    EXEC=$DEFAULT_EXEC
+fi
+
+if [ "${INPUT_CMD}" = 'proxy' ]
+then
+    EXEC=$DEFAULT_EXEC
+fi
+
+if [ "${INPUT_CMD}" = 'apiserver' ]
+then
+    EXEC="apiserver --addr=${DEFAULT_IP}:9092 --addr-http=${DEFAULT_IP}:9093 --discovery $API_SERVER_OPTS"
+fi
+
+if [ "${INPUT_CMD}" = 'etcd' ]
+then
+    EXEC="etcd $ETCD_OPTS"
+fi
+
+if [ ! -z "${INPUT_CMD}" ] && [ -z "$EXEC" ]
+then
+    EXEC=${INPUT_CMD}
+fi
+
+exec $EXEC
